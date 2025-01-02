aa8471465b1496779eeee659d90a3185e8e1d845
ControlsGuild4265 <4265.controlsguild@gmail.com>
2025-01-02 18:34:53 -0500

    Starting command based

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -10,6 +10,10 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.controller.PIDController;
 import frc.robot.Utility.ActuatorInterlocks;
 
+// WPI Command Libraries
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
 public class Elevator {
     public static boolean stowed = true;
     public static boolean climbed = false;
